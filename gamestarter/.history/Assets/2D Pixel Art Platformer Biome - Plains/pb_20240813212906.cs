50430b65a51e351cb5ca5abf0aedf2cfae53e0a3
dl3622 <dl3622@columbia.edu>
2024-08-13 22:03:09 -0400

    dd
    ddddddd

---
+++ gamestarter/.history/Assets/2D Pixel Art Platformer Biome - Plains/pb_20240813212906.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pb : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private RigidBody2D body;
+    private bool grounded;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<RigidBody2d>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {body.velocity = new Vector2(body.velocity.x, speed/2);
+    
+    if( Input.GetKey(KeyCode.Space) && grounded == true ) {
+        body.velocity = new Vector2 (body.velocity.x, speed);
+    
+    grounded = false
+    }
+
+    if( body.velocity.y == 0) {
+        grounded = true;
+    }
+    }    
+    }
+}
